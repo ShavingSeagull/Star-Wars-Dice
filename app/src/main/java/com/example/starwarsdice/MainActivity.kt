b94b4d0321663caa7fda6c31fc2721e603afaceb
Scott C Kipp <scott.ecfx@gmail.com>
2023-03-14 22:19:58 +0000

    Disabled the action bar

--- app/src/main/java/com/example/starwarsdice/MainActivity.kt
+++ app/src/main/java/com/example/starwarsdice/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Disabling the action bar
+        if (supportActionBar != null) {
+            supportActionBar!!.hide()
+        }
     }
 }
