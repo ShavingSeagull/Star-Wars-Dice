259dd8f6e45cff72a1ba682217a2d7d3b998b9d6
Scott C Kipp <scott.ecfx@gmail.com>
2023-03-15 22:55:07 +0000

    Added first pass of new dice roll dialog activity

---
+++ app/src/main/java/com/example/starwarsdice/DiceRollActivity.kt
@@ -0,0 +1,137 @@
+package com.example.starwarsdice
+
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+
+class DiceRollActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val dicePick = intent.getStringExtra("faces")
+        setContentView(R.layout.activity_diceroll)
+        this.setFinishOnTouchOutside(true)
+
+        val closeBtn = findViewById<Button>(R.id.closeButton)
+        closeBtn.setOnClickListener { finish() }
+
+        // Dice face lists, in the order of the sprite sheet
+        val whiteFaces = arrayListOf(
+            R.drawable.white_1_circle_full,
+            R.drawable.white_2_circle_empty,
+            R.drawable.white_1_circle_full,
+            R.drawable.white_2_circle_empty,
+            R.drawable.white_1_circle_full,
+            R.drawable.white_2_circle_empty,
+            R.drawable.white_1_circle_full,
+            R.drawable.white_1_circle_empty,
+            R.drawable.white_1_circle_full,
+            R.drawable.white_1_circle_empty,
+            R.drawable.white_1_circle_full,
+            R.drawable.white_2_circle_full,
+        )
+
+        val redFaces = arrayListOf(
+            R.drawable.red_2_flower,
+            R.drawable.red_1_flower,
+            R.drawable.red_2_flower,
+            R.drawable.red_1_flower,
+            R.drawable.red_1_triangle_and_1_flower,
+            R.drawable.red_1_triangle,
+            R.drawable.red_1_triangle_and_1_flower,
+            R.drawable.red_1_triangle,
+            R.drawable.red_2_triangle,
+            R.drawable.red_1_triangle_circle,
+            R.drawable.red_2_triangle,
+            R.drawable.red_blank,
+        )
+
+        val yellowFaces = arrayListOf(
+            R.drawable.yellow_2_wings,
+            R.drawable.yellow_1_wings,
+            R.drawable.yellow_2_wings,
+            R.drawable.yellow_1_star_circle,
+            R.drawable.yellow_1_star,
+            R.drawable.yellow_1_wings_1_star,
+            R.drawable.yellow_1_star,
+            R.drawable.yellow_1_wings_1_star,
+            R.drawable.yellow_2_star,
+            R.drawable.yellow_1_wings_1_star,
+            R.drawable.yellow_2_star,
+            R.drawable.yellow_blank,
+        )
+
+        val greenFaces = arrayListOf(
+            R.drawable.green_1_star,
+            R.drawable.green_1_wings,
+            R.drawable.green_1_wings_1_star,
+            R.drawable.green_2_stars,
+            R.drawable.green_1_wings,
+            R.drawable.green_1_star,
+            R.drawable.green_2_wings,
+            R.drawable.green_blank,
+        )
+
+        val purpleFaces = arrayListOf(
+            R.drawable.purple_1_flower,
+            R.drawable.purple_1_triangle,
+            R.drawable.purple_1_flower_1_triangle,
+            R.drawable.purple_1_flower,
+            R.drawable.purple_blank,
+            R.drawable.purple_2_flower,
+            R.drawable.purple_2_triangle,
+            R.drawable.purple_1_flower,
+        )
+
+        val blueFaces = arrayListOf(
+            R.drawable.blue_1_star,
+            R.drawable.blue_blank,
+            R.drawable.blue_2_wings,
+            R.drawable.blue_blank,
+            R.drawable.blue_1_wings_1_star,
+            R.drawable.blue_1_wings,
+        )
+
+        val blackFaces = arrayListOf(
+            R.drawable.black_1_triangle,
+            R.drawable.black_1_triangle,
+            R.drawable.black_1_flower,
+            R.drawable.black_1_flower,
+            R.drawable.black_blank,
+            R.drawable.black_blank,
+        )
+
+        val diceRollBackground = findViewById<View>(R.id.diceRollBackground)
+        val diceFaceImage = findViewById<ImageView>(R.id.diceFace)
+        val lightBorderFaces = arrayListOf("redFaces", "purpleFaces", "blackFaces")
+
+        // Sets the background colour for the rice roll result, depending on dice face border colour
+        // Eg: if the border colour is white, the background will be black and vice versa
+        if (dicePick in lightBorderFaces) {
+            diceRollBackground.setBackgroundColor(Color.BLACK)
+        } else {
+            diceRollBackground.setBackgroundColor(Color.WHITE)
+        }
+
+        // Sets the image to reflect what random face was rolled
+        when (dicePick) {
+            "whiteFaces" -> diceFaceImage.setImageResource(diceRoll(whiteFaces))
+            "redFaces" -> diceFaceImage.setImageResource(diceRoll(redFaces))
+            "yellowFaces" -> diceFaceImage.setImageResource(diceRoll(yellowFaces))
+            "greenFaces" -> diceFaceImage.setImageResource(diceRoll(greenFaces))
+            "purpleFaces" -> diceFaceImage.setImageResource(diceRoll(purpleFaces))
+            "blueFaces" -> diceFaceImage.setImageResource(diceRoll(blueFaces))
+            "blackFaces" -> diceFaceImage.setImageResource(diceRoll(blackFaces))
+        }
+
+    }
+
+    private fun diceRoll(faces: ArrayList<Int>): Int {
+        val roll = (0 until faces.size).random()
+        return faces[roll]
+    }
+}
